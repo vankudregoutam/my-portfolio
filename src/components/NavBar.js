9e2f196bf0cf6f9890a1932058865bd65b942b51
unknown <vankudregoutam15@gmail.com>
2024-07-01 17:26:49 +0530

    first commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,8 @@ const NavBar = () => {
         <>
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
-                    <a className="navbar-brand mx-4" href="/"><h2>Goutam Vankudre</h2></a>
+                    <a className="navbar-brand mx-4" href="/"><h2>Goutam Vankudre</h2></a><br />
+                    <p className='role'>Frontend Developer | MERN Stack Developer</p>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon">MENU</span>
                     </button>
